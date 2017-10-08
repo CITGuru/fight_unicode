bcd8d5641e81a466467c8a0fa031ad47ee2444b4
Oyetoke Toby <oyetoketoby@yahoo.com>
2017-10-08 21:39:33 +0100

    Update fight_unicode.py

--- fight_unicode.py
+++ fight_unicode.py
@@ -1,8 +1,8 @@
 __author = "Oyetoke Toby (CITGuru)"
 __version = "0.1"
 
-def safe_unicode(obj, *args):
-    """ this just return the unicode representation of obj """
+def fight_unicode(obj, *args):
+    # this just return the unicode representation of obj
     try:
         return unicode(obj, *args)
     except UnicodeDecodeError:
@@ -10,8 +10,8 @@ def safe_unicode(obj, *args):
         ascii_text = str(obj).encode('string_escape')
         return unicode(ascii_text)
 
-def safe_str(obj):
-    """ this just return the byte string representation of obj """
+def fight_str(obj):
+    # this just return the byte string representation of obj
     try:
         return str(obj)
     except UnicodeEncodeError:
